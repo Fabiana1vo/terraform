ab4fecf516108f427447e05689d11fa33b89ab69
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-06-23 20:32:16 -0300

    fear: meu primeiro commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+# Usa-se a linguagem hcl
+
+//'local_file = provider'
+// 'name' = identificador do arquivo 
+resource "local_file" "exemple" {
+  filename = "exemple1.txt"
+  content = "Hello World!"
+}
