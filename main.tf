2bc8105488922a07612da12776515242891ee1e7
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-06-24 21:27:14 -0300

    feat: add provider configuration

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
 # Usa-se a linguagem hcl
 
+
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+}
+
+
+
 //'local_file = provider'
 // 'name' = identificador do arquivo 
+
 resource "local_file" "exemple" {
   filename = "exemple1.txt"
   content = "Hello Full!"
