377f363fe2c05f2130015a85a7c71eeac8e46be7
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-07-06 17:44:47 -0300

    feat: using random var created

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ terraform {
       version = " ~> 2.1"
     }
 
-      random = {
-      source = "hashicorp/random"
+    random = {
+      source  = "hashicorp/random"
       version = "3.7.2"
     }
   }
@@ -25,16 +25,24 @@ resource "local_file" "exemple" {
 ${var.fruits[0]} é a minha fruta favorita.  
 ${var.fruits[1]} é a fruta que eu mais gosto.
 ${var.fruits[2]} é a fruta que eu menos gosto.
-
-
 Conteúdo do arquivo: ${var.file_content}
 
+Meu pet: ${random_pet.meu_pet.id}
 Frutas de acordo com a aula:
 ${tolist(var.fruits)[1]}
 
 Nome: ${var.person_tuple[0]}
 Idade: ${var.person_tuple[1]}
 Cidade: ${var.person_map.cidade}
-
 EOF
+}
+
+
+
+# Criando um novo resource para utilizar com o random provider
+
+resource "random_pet" "meu_pet" {
+  length    = 3
+  prefix    = "Sr."
+  separator = " "
 }
